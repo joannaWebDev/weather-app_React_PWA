56283ed4036e3e06d087c2e836ff96cc5733bd00
joannaWebDev <joannasmerea@gmail.com>
2020-12-14 19:40:22 +0100

    removed  build from gitignore

---
+++ build/precache-manifest.7400a5ee5adca3a6b176330aa4046c0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05a9c58425fb021e0be79d844f62b64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e63d4bfaf934027bf83f",
+    "url": "/static/css/main.17d707c2.chunk.css"
+  },
+  {
+    "revision": "3696c363641e190a7541",
+    "url": "/static/js/2.d54fc02e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d54fc02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e63d4bfaf934027bf83f",
+    "url": "/static/js/main.5a41253f.chunk.js"
+  },
+  {
+    "revision": "ae594932184f88bfb438",
+    "url": "/static/js/runtime-main.9d9d98d4.js"
+  },
+  {
+    "revision": "80e19d92838adbc8905385af48999139",
+    "url": "/static/media/bg.80e19d92.jpg"
+  }
+]);
